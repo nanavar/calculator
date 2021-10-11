bea4ba77de6fb27706129abfdc40ef4560dae66e
Ana Vargazon <90398111+nanavar@users.noreply.github.com>
2021-10-11 14:34:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 x = int(input("Enter the first number: "))
 y = int(input("Enter the second number: "))
 
-operation = input("Choose math operation (+, -, *, /,): ")
+operation = input("Choose math operation (+, -, *, /): ")
 
 if operation == "+":
     print(x + y)
